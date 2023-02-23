527fdf5de296b810223c65c603000a9588f701b8
Alex Chubaty <achubaty@for-cast.ca>
2023-02-22 21:06:52 -0700

    use replacement for defunct fun

--- CBM_dataPrep_RIApresentDay.R
+++ CBM_dataPrep_RIApresentDay.R
@@ -574,8 +574,9 @@ Init <- function(sim) {
 
 ### changing th order so we can build the PresentDat ageRaster1985
   #4. Make the ecozone Raster (ecoRaster)"http://sis.agr.gc.ca/cansis/nsdb/ecostrat/zone/ecozone_shp.zip"
-  ecozone <- Cache(CBMutils::prepInputsEcozones, url = "http://sis.agr.gc.ca/cansis/nsdb/ecostrat/zone/ecozone_shp.zip",
-                   dPath = dPath,
+  ecozone <- Cache(LandR::prepEcozonesRst,
+                   url = "http://sis.agr.gc.ca/cansis/nsdb/ecostrat/zone/ecozone_shp.zip",
+                   destinationPath = dPath,
                    rasterToMatch = masterRaster)
 
   #5. Get just BC
